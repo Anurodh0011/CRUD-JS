8648dc9bc6e33a734ff09706f7c728c137e0d5f5
Anurodh Prasain <anurodhprasain0011@gmail.com>
2025-02-18 15:21:01 +0545

    index.html updated

--- test.js
+++
@@ -1,5 +0,0 @@
-const num = [1, 2, 3, 4, 5]
-const checkEven = num => num % 2 === 0;
-const hasEvenNum = num.some(checkEven)
-const allEvenNum = num.every(checkEven)
-console.log(hasEvenNum, allEvenNum)
